d2ea561a30002e71d30982f9f272c3905a4a7513
Bashii1646 <hazemst1646@gmail.com>
2026-07-12 21:53:20 +0900

    Create MDXComponents.js

---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MDXComponents from '@theme-original/MDXComponents';
+import Twitter from '@site/src/components/Twitter';
+
+export default {
+  ...MDXComponents,
+  Twitter,
+};
